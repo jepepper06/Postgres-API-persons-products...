c78c8b138040c277f4f550c0fdae56a661b371c5
juansebas <juansebastiancastilloojeda@gmail.com>
2022-07-06 10:15:56 +0800

    Creacion completa del network, paso sigte 1ra prueba

--- components/person/network.js
+++ components/person/network.js
@@ -14,12 +14,39 @@ router.get('/person',(req,res) => {
 })
 
 router.post('/person',(req,res) => {
-    controller.addPerson(req.name,req.country)
+    controller.addPerson(req.body.name,req.body.country)
         .then( addedPerson => {
             response.success(req,res,addedPerson,200)
         }).catch(e => {
             response.error(req,res,'Unexpected Error',500,e)
         }) 
+})
+
+router.patch('/person/:id',(req,res) => {
+    controller.updatePerson(req.params['id'],req.body.name,req.body.country)
+        .then( updatedPerson => {
+            response.success(req,res,updatedPerson,200)
+        }).catch(e => {
+            response.error(req,res,'Unexpected Error',500,e)
+        })
+})
+
+router.get('/person/:country',(req,res) => {
+    controller.getPersonByCountry(req.params['country'])
+        .then( listOfPersons => {
+            response.success(req,res,listOfPersons,200)
+        }).catch( e => {
+            response.error(req,res,'Unexpected Error', 500, e)
+        })
+})
+
+router.get('/person/:id', (req,res) => {
+    controller.getPersonById(req.params['id'])
+        .then( person => {
+            response.success(req,res,person,200)
+        }).catch( e => {
+            response.error(req,res,'Unexpeced Error',500,e)
+        })
 })
 
 router
