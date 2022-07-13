2f76c49812931c26a66a2f1bd0bf37d32fcaaa30
juansebas <juansebastiancastilloojeda@gmail.com>
2022-07-14 00:17:55 +0800

    Dejamos then en store

--- components/person/store.js
+++ components/person/store.js
@@ -3,52 +3,80 @@ const db = require('../../db')
 const pool = db.pool
 
 const ListPersons = async () => {
-    const respuesta = await pool
+    let respuesta
+    await pool
     .query('SELECT name, country FROM person')
+    .then(resp =>{
+        respuesta = resp.rows
+        console.log(resp.rows)
+    })
     .catch(err => {
+        respuesta = err
         setImmediate(
+            
             ()=> {throw err})
     })
-    return respuesta.rows
+    return respuesta
 }
 
 const ListPersonsById = async (id) => {
-    const respuesta = await pool
+    let respuesta
+    await pool
     .query('SELECT name, country FROM person WHERE id = $1',[id])
-    .catch(err => {
-        setImmediate(
-            ()=> {throw err})
+    .then(resp =>{
+        console.log(resp.rows)
+        respuesta = resp.rows
     })
-    return respuesta.rows
+    .catch(err => {
+        const error = `[Error on Store]: ${err}`
+        console.log(error)
+    })
+
+    return respuesta
 }
 
 const ListPersonsByCountry = async (country) => {
-    const respuesta = await pool
+    let respuesta
+    await pool
     .query('SELECT name, country FROM person WHERE country = $1',[country])
-    .catch(err => {
-        setImmediate(
-            ()=> {throw err})
+    .then(resp =>{
+        console.log(resp.rows)
+        respuesta = resp.rows
     })
-    return respuesta.rows
+    .catch(err => {
+        const error = `[Error on Store]: ${err}`
+        console.log(error)
+    })
+    return respuesta
 }
 
 const InsertPerson = async (name,country) => {
-    const respuesta = await pool
+    let respuesta
+    await pool
     .query('INSERT INTO person (name,country) VALUES($1,$2) RETURNING *',[name,country])
+    .then(resp =>{
+        console.log(resp.rows)
+        respuesta = resp.rows
+    })
     .catch(err => {
-        setImmediate(
-            ()=> {throw err})
+        const error = `[Error on Store]: ${err}`
+        console.log(error)
     })
     return respuesta.rows
 }
 
 const updatePerson = async (id,name,country) => {
-    const respuesta = await pool
+    let respuesta
+    await pool
     .query(`UPDATE person  SET name = $1, country = $2
      WHERE id = $3 RETURNING *`,[name,country,id])
+     .then(resp =>{
+        console.log(resp.rows)
+        respuesta = resp.rows
+     })
     .catch(err => {
-        setImmediate(
-            ()=> {throw err})
+        const error = `[Error on Store]: ${err}`
+        console.log(error)
     })
     return respuesta.rows
 }
