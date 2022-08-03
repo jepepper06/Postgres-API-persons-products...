20429c654cfdfb63fbc95ecb1ee837278f1d3858
juansebas <juansebastiancastilloojeda@gmail.com>
2022-08-03 21:55:14 +0800

    Adding Stock Route

--- network/routes.js
+++ network/routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const person = require('../components/person/network')
 const product = require('../components/product/network')
-
+const stock = require('../components/stock/network')
 // MAKING ROUTES AND SETTING MIDDLEWARES
 const routes = function(server){
     server.use('/person',person)
     server.use('/product',product)
+    server.use('/stock',stock)
 }
 module.exports = routes
